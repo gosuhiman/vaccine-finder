1e8dc3b77cc4e658c32742f4d8a10a22d40784b5
gosuhiman <gosuhiman@gmail.com>
2020-10-08 14:02:16 +0200

    tests

---
+++ test/api-client.spec.ts
@@ -0,0 +1,17 @@
+import { GdziePoLekApiClient } from '../src/api-client';
+
+describe('GdziePoLekApiClient', () => {
+  test('Coldrex should be available', async () => {
+    const client = new GdziePoLekApiClient();
+    const result = await client.checkOne({
+      productId: 18983,
+      pvId: 11931,
+      name: 'Coldrex'
+    });
+
+    expect(result).toStrictEqual({
+      name: 'Coldrex',
+      available: true
+    });
+  });
+});
