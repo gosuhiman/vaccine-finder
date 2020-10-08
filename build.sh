c5657ad35bd8372543726dd74158fca4ab9b1a10
gosuhiman <gosuhiman@gmail.com>
2020-10-08 13:32:25 +0200

    cd fixes

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ./build
-npm run build
-cp -r ./node_modules ./build
-
-# TODO zip
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-# aws lambda update-function-code --function-name vaccineFinder --zip-file fileb://build/vaccine-finder.zip --region eu-central-1
